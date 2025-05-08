d90ff748a72d33ce0919340cb2e23dfcd849c031
João Markus <markusjoaomarkus99@gmail.com>
2025-05-08 14:23:46 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Academia_Programador_GestaoEquipamentosFabricantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
